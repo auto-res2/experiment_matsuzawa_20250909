8124cb2852263fbad05be0a15aafe59d4cb1e260
Takumi Matsuzawa <152503584+genga6@users.noreply.github.com>
2025-09-09 19:55:04 +0900

    Fix code issues for iteration 23

--- src/train.py
+++ src/train.py
@@ -131,7 +131,7 @@ from utils import dump_json  # noqa: E402 – local util
 
 
 # ---------------------------- CONSTANT PATHS ------------------------------
-_ITER_DIR = pathlib.Path(".research/iteration22")
+_ITER_DIR = pathlib.Path(".research/iteration23")
 _IMG_DIR = _ITER_DIR / "images"
 
 
